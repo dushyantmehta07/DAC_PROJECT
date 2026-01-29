eabb740c929aeb27b5a84d912d80b2064d37b660
dushyantmehta07 <dushyantmehta07@gmail.com>
2026-01-29 13:43:25 +0530

    Delete PROJECT directory

--- PROJECT/backend/src/main/java/com/ecommerce/backend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.backend.repository;
-
-import com.ecommerce.backend.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- PROJECT/frontend/src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h3>Loading...</h3>
-    </div>
-  );
-};
-
-export default Loader;
