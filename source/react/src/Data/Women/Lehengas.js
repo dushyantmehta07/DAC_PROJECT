e1a63ee173aa80e8e8d5086fdc1d12b6712786ec
Manaswini Mulik <mulikmanaswini@gmail.com>
2026-01-29 14:55:42 +0530

    New Section added into HomePage

---
+++ source/react/src/Data/Women/Lehengas.js
@@ -0,0 +1,152 @@
+export const lehenga_page1 = [
+  {
+    imageUrl:
+      "https://rukminim1.flixcart.com/image/612/612/xif0q/lehenga-choli/u/g/a/free-half-sleeve-vbnd-fddj-astonblue-original-imagp286bmcaabbj.jpeg?q=70",
+    brand: "AstonBlue",
+    title: "Self Design Semi Stitched Lehengas",
+    color: "Yellow",
+    discountedPrice: 35999,
+    price: 39999,
+    discountPersent: 10,
+    size: [
+      { name: "S", quantity: 20 },
+      { name: "M", quantity: 30 },
+      { name: "L", quantity: 50 },
+    ],
+    description:
+      "Yellow Readymade Lehenga in Net fabric embellished with Mirrorwork embroidery.",
+    topLavelCategory: "Women",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "lehenga_choli",
+    quantity: 100,
+  },
+
+  {
+    imageUrl:
+      "https://rukminim1.flixcart.com/image/612/612/kmz7qfk0/lehenga-choli/m/0/t/s-sleeveless-l032-rayie-collection-original-imagfrbqhvddrnbd.jpeg?q=70",
+    brand: "Rayie Collection",
+    title: "Embroidered Stitched Lehenga & Crop Top",
+    color: "Black",
+    discountedPrice: 19124,
+    price: 35999,
+    discountPersent: 46,
+    size: [
+      { name: "S", quantity: 20 },
+      { name: "M", quantity: 30 },
+      { name: "L", quantity: 50 },
+    ],
+    description:
+      "Net fabric lehenga with mirrorwork embroidery and stitched blouse.",
+    topLavelCategory: "Women",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "lehenga_choli",
+    quantity: 100,
+  },
+
+  {
+    imageUrl:
+      "https://rukminim1.flixcart.com/image/612/612/xif0q/lehenga-choli/5/n/h/l-3-4-sleeve-black-liva-original-imagmeyw2zzbch2g.jpeg?q=70",
+    brand: "Liva",
+    title: "Embroidered Stitched Lehenga & Crop Top",
+    color: "Black",
+    discountedPrice: 13999,
+    price: 20000,
+    discountPersent: 30,
+    size: [
+      { name: "S", quantity: 20 },
+      { name: "M", quantity: 30 },
+      { name: "L", quantity: 50 },
+    ],
+    description:
+      "Elegant black lehenga with mirror embroidery and ready blouse.",
+    topLavelCategory: "Women",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "lehenga_choli",
+    quantity: 100,
+  },
+
+  {
+    imageUrl:
+      "https://rukminim1.flixcart.com/image/612/612/xif0q/lehenga-choli/b/h/z/xl-sleeveless-owa2cd2320a-soch-original-imagjvmyhy63ms7z.jpeg?q=70",
+    brand: "Soch",
+    title: "Embellished Stitched Lehenga Choli",
+    color: "Yellow",
+    discountedPrice: 12998,
+    price: 18999,
+    discountPersent: 32,
+    size: [
+      { name: "S", quantity: 20 },
+      { name: "M", quantity: 30 },
+      { name: "L", quantity: 50 },
+    ],
+    description: "Festive yellow lehenga with rich embellishments.",
+    topLavelCategory: "Women",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "lehenga_choli",
+    quantity: 100,
+  },
+
+  {
+    imageUrl:
+      "https://rukminim1.flixcart.com/image/612/612/xif0q/lehenga-choli/y/d/8/l-3-4-sleeve-green-liva-original-imagmez3duzyzzxz.jpeg?q=70",
+    brand: "Liva",
+    title: "Embroidered Stitched Lehenga & Crop Top",
+    color: "Green",
+    discountedPrice: 9999,
+    price: 18000,
+    discountPersent: 44,
+    size: [
+      { name: "S", quantity: 20 },
+      { name: "M", quantity: 30 },
+      { name: "L", quantity: 50 },
+    ],
+    description:
+      "Green lehenga with mirror embroidery and matching dupatta.",
+    topLavelCategory: "Women",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "lehenga_choli",
+    quantity: 100,
+  },
+
+  {
+    imageUrl:
+      "https://rukminim1.flixcart.com/image/612/612/xif0q/lehenga-choli/j/b/g/xl-sleeveless-owa2cd2319a-soch-original-imagjvmysh3z9egk.jpeg?q=70",
+    brand: "Soch",
+    title: "Solid Stitched Lehenga Choli",
+    color: "Dark Blue",
+    discountedPrice: 9998,
+    price: 14999,
+    discountPersent: 30,
+    size: [
+      { name: "S", quantity: 20 },
+      { name: "M", quantity: 30 },
+      { name: "L", quantity: 50 },
+    ],
+    description: "Classic solid lehenga choli with premium finish.",
+    topLavelCategory: "Women",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "lehenga_choli",
+    quantity: 100,
+  },
+
+  {
+    imageUrl:
+      "https://rukminim1.flixcart.com/image/612/612/xif0q/lehenga-choli/o/9/i/4xl-short-sleeve-dk0819-divena-original-imagjwp9mh2urcqg.jpeg?q=70",
+    brand: "DIVENA",
+    title: "Printed Stitched Lehenga Choli",
+    color: "Red",
+    discountedPrice: 4499,
+    price: 11999,
+    discountPersent: 62,
+    size: [
+      { name: "S", quantity: 20 },
+      { name: "M", quantity: 30 },
+      { name: "L", quantity: 50 },
+    ],
+    description: "Printed red lehenga perfect for festive occasions.",
+    topLavelCategory: "Women",
+    secondLavelCategory: "Clothing",
+    thirdLavelCategory: "lehenga_choli",
+    quantity: 100,
+  },
+];
--- source/react/src/Pages/Homepage.jsx
+++ source/react/src/Pages/Homepage.jsx
@@ -4,11 +4,11 @@ import HomeProductSection from "../customer/Components/Home/HomeProductSection";
 
 import { sareePage1 } from "../Data/Saree/page1";
 import { dressPage1 } from "../Data/dress/page1";
-import { gounsPage1 } from "../Data/Gouns/gouns";
+import { nightsuitsPage1 } from "../Data/NightSuits/nightsuits";
 import { kurtaPage1 } from "../Data/Kurta/kurta";
 import { mensShoesPage1 } from "../Data/shoes";
 import { mens_kurta } from "../Data/Men/men_kurta";
-import { lengha_page1 } from "../Data/Women/LenghaCholi";
+import { lehenga_page1 } from "../Data/Women/Lehengas";
 
 const Homepage = () => {
   return (
@@ -22,10 +22,10 @@ const Homepage = () => {
       <div className="space-y-10 py-20">
         <HomeProductSection data={mens_kurta} section={"Men's Kurta"} />
         <HomeProductSection data={mensShoesPage1} section={"Men's Shoes"} />
-        <HomeProductSection data={lengha_page1} section={"Lengha Choli"} />
+        <HomeProductSection data={lehenga_page1} section={"Lehengas"} />
         <HomeProductSection data={sareePage1} section={"Saree"} />
         <HomeProductSection data={dressPage1} section={"Dress"} />
-        <HomeProductSection data={gounsPage1} section={"Women's Gouns"} />
+        <HomeProductSection data={nightsuitsPage1} section={"Women's NightSuits"} />
         <HomeProductSection data={kurtaPage1} section={"Women's Kurtas"} />
       </div>
     </div>
